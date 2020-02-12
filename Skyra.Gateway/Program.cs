d1f190f1fc2467486fa38f76ce51f67f863d5faa
Antonio Román <kyradiscord@gmail.com>
2020-02-12 18:42:02 +0100

    src: Progress

--- Skyra.Gateway/Program.cs
+++ Skyra.Gateway/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Skyra.Gateway.Core;
+using Spectacles.NET.Gateway;
+using Spectacles.NET.Types;
 
 namespace Skyra.Gateway
 {
-	class Program
+	public static class Program
 	{
-		static void Main(string[] args)
+		public static async Task Main()
 		{
-			Console.WriteLine("Hello World!");
+			var token = Environment.GetEnvironmentVariable("DISCORD_TOKEN");
+			var brokerName = Environment.GetEnvironmentVariable("BROKER_NAME");
+			var brokerUrl = Environment.GetEnvironmentVariable("BROKER_URL");
+			var shardCount = Environment.GetEnvironmentVariable("DISCORD_SHARD_COUNT") ?? "";
+
+			if (token == null || brokerName == null || brokerUrl == null)
+				throw new SystemException("Missing core arguments");
+
+			var identifyOptions = new IdentifyOptions
+			{
+				GuildSubscriptions = true,
+				LargeThreshold = 250,
+				Presence = new UpdateStatusDispatch {Status = "Skyra, help"}
+			};
+
+			var gateway = new GatewayHandler(token,
+				brokerName,
+				new Uri(brokerUrl),
+				identifyOptions,
+				int.Parse(shardCount));
+
+			await gateway.ConnectAsync();
 		}
 	}
 }

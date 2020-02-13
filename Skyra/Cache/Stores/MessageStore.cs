dbcceb5acbd996b5b9d272c400f162cc0f86dd5c
Antonio Román <kyradiscord@gmail.com>
2020-02-14 00:28:36 +0100

    feat: Added code to store messages in Redis

--- Skyra/Cache/Stores/MessageStore.cs
+++ Skyra/Cache/Stores/MessageStore.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Spectacles.NET.Types;
+using StackExchange.Redis;
 
 namespace Skyra.Cache.Stores
 {
@@ -10,9 +11,26 @@ namespace Skyra.Cache.Stores
 		{
 		}
 
-		public override Task SetAsync(Message entry, string? parent = null)
+		public override async Task SetAsync(Message entry, string? parent = null)
 		{
-			throw new System.NotImplementedException();
+			var temporaryMessage = new Message
+			{
+				Id = entry.Id,
+				Content = entry.Content,
+				Embeds = entry.Embeds,
+				Type = entry.Type,
+				Timestamp = entry.Timestamp,
+				ChannelId = entry.ChannelId,
+				EditedTimestamp = entry.EditedTimestamp,
+				GuildId = entry.GuildId,
+				WebhookId = entry.WebhookId
+			};
+
+			await Task.WhenAll(
+				Client.Users.SetAsync(entry.Author),
+				Client.Members.SetAsync(entry.Member),
+				Database.HashSetAsync(FormatKeyName(parent),
+					new[] {new HashEntry(entry.Id, SerializeValue(temporaryMessage))}));
 		}
 
 		public override Task SetAsync(IEnumerable<Message> entries, string? parent = null)

defee2252f369101f4eb1bc4dc8772c2ae6a6cc5
Tyler Davis <tgfdavis98@gmail.com>
2020-02-13 00:55:05 +0000

    apply resharpers refactorings

--- Skyra.Framework/Structures/Monitor.cs
+++ Skyra.Framework/Structures/Monitor.cs
@@ -8,35 +8,35 @@ namespace Skyra.Framework.Structures
 	public abstract class Monitor : Piece
 	{
 		public readonly Client Client;
-		private readonly MessageType[] AllowedTypes;
-		private readonly bool IgnoreBots;
-		private readonly bool IgnoreSelf;
-		private readonly bool IgnoreOthers;
-		private readonly bool IgnoreWebhooks;
-		private readonly bool IgnoreEdits;
+		private readonly MessageType[] _allowedTypes;
+		private readonly bool _ignoreBots;
+		private readonly bool _ignoreSelf;
+		private readonly bool _ignoreOthers;
+		private readonly bool _ignoreWebhooks;
+		private readonly bool _ignoreEdits;
 
 		protected Monitor(Client client, MonitorOptions options)
 		{
 			Client = client;
 			Name = options.Name;
-			AllowedTypes = options.AllowedTypes;
-			IgnoreBots = options.IgnoreBots;
-			IgnoreSelf = options.IgnoreSelf;
-			IgnoreOthers = options.IgnoreOthers;
-			IgnoreWebhooks = options.IgnoreWebhooks;
-			IgnoreEdits = options.IgnoreEdits;
+			_allowedTypes = options.AllowedTypes;
+			_ignoreBots = options.IgnoreBots;
+			_ignoreSelf = options.IgnoreSelf;
+			_ignoreOthers = options.IgnoreOthers;
+			_ignoreWebhooks = options.IgnoreWebhooks;
+			_ignoreEdits = options.IgnoreEdits;
 		}
 
 		public abstract Task<bool> Run(Message message);
 
 		public bool ShouldRun(Message message)
 		{
-			return AllowedTypes.Contains(message.Type)
-			       && !(IgnoreBots && message.Author.Bot)
+			return _allowedTypes.Contains(message.Type)
+			       && !(_ignoreBots && message.Author.Bot)
 			       // && !(IgnoreSelf && message.Author.Id == Client.User.Id)
 			       // && !(IgnoreOthers && message.Author.Id != Client.User.Id)
-			       && !(IgnoreWebhooks && message.WebhookId != null)
-			       && !(IgnoreEdits && message.EditedTimestamp != null);
+			       && !(_ignoreWebhooks && message.WebhookId != null)
+			       && !(_ignoreEdits && message.EditedTimestamp != null);
 		}
 	}
 

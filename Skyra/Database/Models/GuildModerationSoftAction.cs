c347fd31d09d951e19d97acf20a2ce7f0a49ea3c
Antonio Román <kyradiscord@gmail.com>
2020-02-18 23:46:14 +0100

    feat: Added setters to GuildModerationSoftAction

--- Skyra/Database/Models/GuildModerationSoftAction.cs
+++ Skyra/Database/Models/GuildModerationSoftAction.cs
@@ -2,22 +2,37 @@ namespace Skyra.Database.Models
 {
 	public struct GuildModerationSoftAction
 	{
-		public byte Bitfield { get; }
+		/// <summary>
+		///     The raw bitfield number
+		/// </summary>
+		public byte Bitfield { get; private set; }
 
 		/// <summary>
 		///     Whether or not the soft action should issue a warning on the channel.
 		/// </summary>
-		public bool Alert => (Bitfield & 0x001) == 0x001;
+		public bool Alert
+		{
+			get => (Bitfield & 0b001) == 0b001;
+			set => Bitfield = (byte) (value ? Bitfield | 0b001 : Bitfield & 0b110);
+		}
 
 		/// <summary>
 		///     Whether or not the infraction should be logged in <see cref="GuildChannel.ModerationLogs" />.
 		/// </summary>
-		public bool Log => (Bitfield & 0x010) == 0x010;
+		public bool Log
+		{
+			get => (Bitfield & 0b010) == 0b010;
+			set => Bitfield = (byte) (value ? Bitfield | 0b010 : Bitfield & 0b101);
+		}
 
 		/// <summary>
 		///     Whether or not the message should be deleted.
 		/// </summary>
-		public bool Delete => (Bitfield & 0x100) == 0x100;
+		public bool Delete
+		{
+			get => (Bitfield & 0b100) == 0b100;
+			set => Bitfield = (byte) (value ? Bitfield | 0b100 : Bitfield & 0b011);
+		}
 
 		/// <summary>
 		///     Construct this instance from a bitfield.

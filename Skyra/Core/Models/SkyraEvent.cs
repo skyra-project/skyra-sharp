98e3246bfaec921c1f2588279ef61cb8a69b66f6
QuantumlyTangled <ethoarx@gmail.com>
2020-02-23 19:19:56 +0100

    Requested changes

--- Skyra/Core/Models/SkyraEvent.cs
+++ Skyra/Core/Models/SkyraEvent.cs
@@ -3,199 +3,199 @@ namespace Skyra.Core.Models
 	public enum SkyraEvent
 	{
 		/// <summary>
-		///     contains the initial state information
+		///     Contains the initial state information.
 		/// </summary>
 		READY,
 
 		/// <summary>
-		///     response to Resume
+		///     Response to Resume.
 		/// </summary>
 		RESUMED,
 
 		/// <summary>
-		///     new channel created
+		///     New channel created.
 		/// </summary>
 		CHANNEL_CREATE,
 
 		/// <summary>
-		///     channel was updated
+		///     Channel was updated.
 		/// </summary>
 		CHANNEL_UPDATE,
 
 		/// <summary>
-		///     channel was deleted
+		///     Channel was deleted.
 		/// </summary>
 		CHANNEL_DELETE,
 
 		/// <summary>
-		///     message was pinned or unpinned
+		///     Message was pinned or unpinned.
 		/// </summary>
 		CHANNEL_PINS_UPDATE,
 
 		/// <summary>
-		///     lazy-load for unavailable guild, guild became available, or user joined a new guild
+		///     Lazy-load for unavailable guild, guild became available, or user joined a new guild.
 		/// </summary>
 		GUILD_CREATE,
 
 		/// <summary>
-		///     guild was updated
+		///     Guild was updated.
 		/// </summary>
 		GUILD_UPDATE,
 
 		/// <summary>
-		///     guild became unavailable, or user left/was removed from a guild
+		///     Guild became unavailable, or user left/was removed from a guild.
 		/// </summary>
 		GUILD_DELETE,
 
 		/// <summary>
-		///     user was banned from a guild
+		///     User was banned from a guild.
 		/// </summary>
 		GUILD_BAN_ADD,
 
 		/// <summary>
-		///     user was unbanned from a guild
+		///     User was unbanned from a guild.
 		/// </summary>
 		GUILD_BAN_REMOVE,
 
 		/// <summary>
-		///     guild emojis were updated
+		///     Guild emojis were updated.
 		/// </summary>
 		GUILD_EMOJIS_UPDATE,
 
 		/// <summary>
-		///     guild integration was updated
+		///     Guild integration was updated.
 		/// </summary>
 		GUILD_INTEGRATIONS_UPDATE,
 
 		/// <summary>
-		///     new user joined a guild
+		///     New user joined a guild.
 		/// </summary>
 		GUILD_MEMBER_ADD,
 
 		/// <summary>
-		///     user was removed from a guild
+		///     User was removed from a guild.
 		/// </summary>
 		GUILD_MEMBER_REMOVE,
 
 		/// <summary>
-		///     guild member was updated
+		///     Guild member was updated.
 		/// </summary>
 		GUILD_MEMBER_UPDATE,
 
 		/// <summary>
-		///     response to Request Guild Members
+		///     Response to Request Guild Members.
 		/// </summary>
 		GUILD_MEMBERS_CHUNK,
 
 		/// <summary>
-		///     guild role was created
+		///     Guild role was created.
 		/// </summary>
 		GUILD_ROLE_CREATE,
 
 		/// <summary>
-		///     guild role was updated
+		///     Guild role was updated.
 		/// </summary>
 		GUILD_ROLE_UPDATE,
 
 		/// <summary>
-		///     guild role was deleted
+		///     Guild role was deleted.
 		/// </summary>
 		GUILD_ROLE_DELETE,
 
 		/// <summary>
-		/// 	invite to a channel was created
+		/// 	Invite to a channel was created.
 		/// </summary>
 		INVITE_CREATE,
 
 		/// <summary>
-		/// 	invite to a channel was deleted
+		/// 	Invite to a channel was deleted.
 		/// </summary>
 		INVITE_DELETE,
 
 		/// <summary>
-		///     message was created
+		///     Message was created.
 		/// </summary>
 		MESSAGE_CREATE,
 
 		/// <summary>
-		///     message was edited
+		///     Message was edited.
 		/// </summary>
 		MESSAGE_UPDATE,
 
 		/// <summary>
-		///     message was deleted
+		///     Message was deleted.
 		/// </summary>
 		MESSAGE_DELETE,
 
 		/// <summary>
-		///     multiple messages were deleted at once
+		///     Multiple messages were deleted at once.
 		/// </summary>
 		MESSAGE_DELETE_BULK,
 
 		/// <summary>
-		///     user reacted to a message
+		///     User reacted to a message.
 		/// </summary>
 		MESSAGE_REACTION_ADD,
 
 		/// <summary>
-		///     user removed a reaction from a message
+		///     User removed a reaction from a message.
 		/// </summary>
 		MESSAGE_REACTION_REMOVE,
 
 		/// <summary>
-		///     all reactions were explicitly removed from a message
+		///     All reactions were explicitly removed from a message.
 		/// </summary>
 		MESSAGE_REACTION_REMOVE_ALL,
 
 		/// <summary>
-		/// 	all reactions for a given emoji were explicitly removed from a message
+		/// 	All reactions for a given emoji were explicitly removed from a message.
 		/// </summary>
 		MESSAGE_REACTION_REMOVE_EMOJI,
 
 		/// <summary>
-		///     user was updated
+		///     User was updated.
 		/// </summary>
 		PRESENCE_UPDATE,
 
 		/// <summary>
-		///     used to replace presences after outages, data is null for bot accounts receiving it
-		///     <remarks>Bot Accounts should ignore this</remarks>
+		///     Used to replace presences after outages, data is null for bot accounts receiving it.
+		///     <remarks>Bot Accounts should ignore this.</remarks>
 		/// </summary>
 		PRESENCES_REPLACE,
 
 		/// <summary>
-		///     user started typing in a channel
+		///     User started typing in a channel.
 		/// </summary>
 		TYPING_START,
 
 		/// <summary>
-		///     properties about the user changed
+		///     Properties about the user changed.
 		/// </summary>
 		USER_UPDATE,
 
 		/// <summary>
-		///     someone joined, left, or moved a voice channel
+		///     Someone joined, left, or moved a voice channel.
 		/// </summary>
 		VOICE_STATE_UPDATE,
 
 		/// <summary>
-		///     guild's voice server was updated
+		///     Guild's voice server was updated.
 		/// </summary>
 		VOICE_SERVER_UPDATE,
 
 		/// <summary>
-		///     guild channel webhook was created, update, or deleted
+		///     Guild channel webhook was created, update, or deleted.
 		/// </summary>
 		WEBHOOKS_UPDATE,
 
 		/// <summary>
-		/// a twitch stream started
+		/// 	A twitch stream has started.
 		/// </summary>
 
 		NOTIFY_TWITCH_STREAM_START,
 
 		/// <summary>
-		/// a twitch stream ended
+		/// 	A twitch stream has ended.
 		/// </summary>
 
 		NOTIFY_TWITCH_STREAM_END

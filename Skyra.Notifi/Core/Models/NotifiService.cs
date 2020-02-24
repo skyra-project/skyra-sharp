2ea54a9b81cf7cfd6e980a3a651a78d514b563d5
Antonio Román <kyradiscord@gmail.com>
2020-02-24 01:52:50 +0100

    cleanup: Performed cleanup sweep from Rider

--- Skyra.Notifi/Core/Models/NotifiService.cs
+++ Skyra.Notifi/Core/Models/NotifiService.cs
@@ -6,9 +6,6 @@ namespace Skyra.NotifI.Core.Models
 {
 	public class NotifiService
 	{
-		public string TwitchToken { get; }
-		public AmqpBroker Broker { get; }
-
 		private readonly Uri _brokerUrl;
 
 		public NotifiService()
@@ -16,11 +13,14 @@ namespace Skyra.NotifI.Core.Models
 			var brokerName = Environment.GetEnvironmentVariable("BROKER_NAME") ?? "skyra";
 			var brokerUrl = Environment.GetEnvironmentVariable("BROKER_URL") ?? "amqp://localhost:5672";
 			TwitchToken = Environment.GetEnvironmentVariable("TWITCH_TOKEN") ??
-			              throw new ArgumentNullException($"TWITCH_TOKEN");
+			              throw new ArgumentNullException("TWITCH_TOKEN");
 
 			Broker = new AmqpBroker(brokerName);
 			_brokerUrl = new Uri(brokerUrl);
 		}
+
+		public string TwitchToken { get; }
+		public AmqpBroker Broker { get; }
 
 		public async Task StartAsync()
 		{
--- Skyra.Notifi/Program.cs
+++ Skyra.Notifi/Program.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Skyra.NotifI.Core.Models;
 
 namespace Skyra.Notifi
@@ -26,8 +21,10 @@ namespace Skyra.Notifi
 			await webHost.RunAsync();
 		}
 
-		public static IHostBuilder CreateHostBuilder(string[] args) =>
-			Host.CreateDefaultBuilder(args)
+		public static IHostBuilder CreateHostBuilder(string[] args)
+		{
+			return Host.CreateDefaultBuilder(args)
 				.ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+		}
 	}
 }
